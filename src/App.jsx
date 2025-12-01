efee60059322948adecc1c312ff9cdbba3caae53
Beresnandor <beresnandor2007@gmail.com>
2025-12-01 13:11:36 +0100

    add to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import HomePage from './components/home-page/HomePage.jsx'
 
 function App() {
   
 
   return (
     <>
-      
+      <HomePage />
     </>
   )
 }
