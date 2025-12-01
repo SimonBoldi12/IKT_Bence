28e0691dd9333e70af7e5b176f346b05c3c7786b
Beresnandor <beresnandor2007@gmail.com>
2025-12-01 13:29:44 +0100

    oldal hibaelhárítása

--- src/components/home-page/HomePage.jsx
+++ src/components/home-page/HomePage.jsx
@@ -1,28 +1,44 @@
 import styles from './HomePage.module.css';
+import futbinLogo from '../../assets/futbin.png';
+import futbinLogo2 from '../../assets/futbin (2).png';
+import reveszImg from '../../assets/Révész.png';
+import kristofImg from '../../assets/Kristóf.png';
+import fraterImg from '../../assets/Fráter.png';
+import medveImg from '../../assets/Medve.png';
+import hensiImg from '../../assets/Hensi.png';
+import baloghImg from '../../assets/Balogh.png';
+import szaboImg from '../../assets/Szabó (2).png';
+import rehoImg from '../../assets/Rehó.png';
+import rodriImg from '../../assets/rodri.png';
+import hanImg from '../../assets/han.png';
+import bellImg from '../../assets/bell.png';
+import vanImg from '../../assets/van.png';
+import haalImg from '../../assets/haal.png';
+import hakimiImg from '../../assets/hakimi.png';
 
 function HomePage() {
     const players = [
-        { src: './assets/Révész.png', link: './players/revesz.html' },
-        { src: './assets/Kristóf.png' },
-        { src: './assets/Fráter.png' },
-        { src: './assets/Medve.png' },
-        { src: './assets/Hensi.png' },
-        { src: './assets/Balogh.png' },
-        { src: './assets/Szabó (2).png' },
-        { src: './assets/Rehó.png' },
-        { src: './assets/rodri.png' },
-        { src: './assets/han.png' },
-        { src: './assets/bell.png' },
-        { src: './assets/van.png' },
-        { src: './assets/haal.png' },
-        { src: './assets/hakimi.png' },
+        { src: reveszImg, alt: 'Révész', link: '/players' },
+        { src: kristofImg, alt: 'Kristóf' },
+        { src: fraterImg, alt: 'Fráter' },
+        { src: medveImg, alt: 'Medve' },
+        { src: hensiImg, alt: 'Hensi' },
+        { src: baloghImg, alt: 'Balogh' },
+        { src: szaboImg, alt: 'Szabó' },
+        { src: rehoImg, alt: 'Rehó' },
+        { src: rodriImg, alt: 'Rodri' },
+        { src: hanImg, alt: 'Han' },
+        { src: bellImg, alt: 'Bell' },
+        { src: vanImg, alt: 'Van' },
+        { src: haalImg, alt: 'Haal' },
+        { src: hakimiImg, alt: 'Hakimi' },
     ];
 
     return (
         <div className={styles.body}>
-            <nav>
+            <nav className={styles.nav}>
                 <div className={styles.logo}>
-                    <img src="./assets/futbin.png" alt="" />
+                    <img src={futbinLogo} alt="Futbin Logo" />
                 </div>
                 <div className={styles.menu}>
                     <a href="">Players</a>
@@ -31,12 +47,12 @@ function HomePage() {
                     <a href="">Market</a>
                     <a href="">Evolutions</a>
                 </div>
-                <button id="btn1">Log in</button>
+                <button className={styles.btn1}>Log in</button>
             </nav>
             <div className={styles.container}>
                 <div className={styles.keresosav}>
                     <div className={styles.kep}>
-                        <img src="./assets/futbin (2).png" alt="" />
+                        <img src={futbinLogo2} alt="Futbin Logo 2" />
                     </div>
                     <input id="kereses" type="text" placeholder="Search for EA FC26 player..." />
                 </div>
@@ -49,21 +65,15 @@ function HomePage() {
                     <p>Cheapest by rating</p>
                 </div>
                 <div className={styles.kartyak}>
-                    <a href="./players/revesz.html">
-                    <img src="./assets/Révész.png" alt="" /></a>
-                    <img src="./assets/Kristóf.png" alt="" />
-                    <img src="./assets/Fráter.png" alt="" />
-                    <img src="./assets/Medve.png" alt="" />
-                    <img src="./assets/Hensi.png" alt="" />
-                    <img src="./assets/Balogh.png" alt="" />
-                    <img src="./assets/Szabó (2).png" alt="" />
-                    <img src="./assets/Rehó.png" alt="" />
-                    <img src="./assets/rodri.png" alt="" />
-                    <img src="./assets/han.png" alt="" />
-                    <img src="./assets/bell.png" alt="" />
-                    <img src="./assets/van.png" alt="" />
-                    <img src="./assets/haal.png" alt="" />
-                    <img src="./assets/hakimi.png" alt="" />
+                    {players.map((player, index) => (
+                        player.link ? (
+                            <a key={index} href={player.link}>
+                                <img src={player.src} alt={player.alt} />
+                            </a>
+                        ) : (
+                            <img key={index} src={player.src} alt={player.alt} />
+                        )
+                    ))}
                 </div>
                 <div className={styles.popular}>
                     <a href="https://www.futbin.com/popular" target="_blank" rel="noopener noreferrer">
